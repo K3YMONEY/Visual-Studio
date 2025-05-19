302db54d3edf9811dd84fbd48e5d3225a62e0ab8
K3YMONEY <wairimuiruri21@gmail.com>
2025-05-19 16:51:33 +0300

    Creating Dictionaries

---
+++ Dictionaries/1-create_dictionary.py
@@ -0,0 +1,9 @@
+#create a dictionary of an employee with first name, last name,ID,email and organisation.
+Employee_info = {
+    'first name' : 'Robert',
+    'last name' : 'muiruri',
+    'ID' : 12345678,
+    'Email' : 'rober32@gmail.com',
+    'Organisation' : 'KMD'
+}
+print(Employee_info)
