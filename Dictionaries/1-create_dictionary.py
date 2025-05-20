1ac76175384b826ef8d028f171873a6db14201f0
K3YMONEY <wairimuiruri21@gmail.com>
2025-05-20 16:36:32 +0300

    updated_files

--- Dictionaries/1-create_dictionary.py
+++ Dictionaries/1-create_dictionary.py
@@ -1,9 +1,16 @@
 #create a dictionary of an employee with first name, last name,ID,email and organisation.
 Employee_info = {
-    'first name' : 'Robert',
-    'last name' : 'muiruri',
+    'first_name' : 'Robert',
+    'last_name' : 'muiruri',
     'ID' : 12345678,
     'Email' : 'rober32@gmail.com',
     'Organisation' : 'KMD'
 }
+print(Employee_info)
+print(Employee_info['last_name'])
+Employee_info['position'] = 'Manager'
+print(Employee_info)
+del Employee_info['last_name']
+print(Employee_info)
+Employee_info.clear()
 print(Employee_info)
